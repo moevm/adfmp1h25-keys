399092e61f79b9dae7364e51744dfda081e8b098
Ajems <danilkorenev2004@gmail.com>
2025-02-18 00:45:23 +0300

    add icon and splash screen color

--- Duplikeytor/app/build.gradle.kts
+++ Duplikeytor/app/build.gradle.kts
@@ -49,6 +49,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(libs.androidx.core.splashscreen)
+
+    // tests
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ Duplikeytor/app/src/main/java/ru/etu/duplikeytor/presentation/uiKit/theme/Color.kt
@@ -0,0 +1,13 @@
+package ru.etu.duplikeytor.presentation.uiKit.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue = Color(0xFF376CE1)
+val Yellow = Color(0xFFF6C84A)
+
+val DarkGrey = Color(0xFF141414)
+val LightGrey = Color(0xFF1E1E1E)
+
+val FillWhite = Color(0xFFFFFFFF)
+val NighWhite = Color(0xFFFAFAFA)
+val FadeWhite = Color(0xFFE6E6E6)
