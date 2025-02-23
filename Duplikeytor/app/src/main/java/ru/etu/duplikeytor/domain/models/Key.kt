558312c082f123ab292dd8ba50181ded5768b482
Dmitriy Smirnov <dyusmirnov@stud.etu.ru>
2025-02-23 23:20:18 +0300

    feat: add key model

---
+++ Duplikeytor/app/src/main/java/ru/etu/duplikeytor/domain/models/Key.kt
@@ -0,0 +1,28 @@
+package ru.etu.duplikeytor.domain.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import ru.etu.duplikeytor.domain.converters.Converters
+import java.util.Date
+
+@Entity(tableName = "keys")
+@TypeConverters(Converters::class)
+data class Key(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name="key_id")
+    var id: Long,
+
+    var name: String,
+
+    @ColumnInfo(name="photo_uri")
+    var photoUri: String?,
+
+    var type: String,
+
+    var pins: List<Int>,
+
+    @ColumnInfo(name="created_at")
+    var createdAt: String,
+)
