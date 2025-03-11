38130e849dbf2c13b3206b2d799a90b344ab54b4
Dmitriy Smirnov <dyusmirnov@stud.etu.ru>
2025-03-11 16:52:07 +0300

    fix: add field id

--- Duplikeytor/app/src/main/java/ru/etu/duplikeytor/presentation/archive/keycard/KeyState.kt
+++ Duplikeytor/app/src/main/java/ru/etu/duplikeytor/presentation/archive/keycard/KeyState.kt
@@ -3,6 +3,7 @@ package ru.etu.duplikeytor.presentation.archive.keycard
 import ru.etu.duplikeytor.presentation.shared.model.KeyType
 
 internal data class KeyState(
+    val id: Long,
     val name: String,
     val imageUri: String?,
     val createdAt: String,
